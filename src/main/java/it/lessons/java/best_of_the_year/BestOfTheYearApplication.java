6dcdda9af7a92789b3b76f6af135dc27d743521a
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-10-30 09:28:53 +0100

    Initial commit

---
+++ src/main/java/it/lessons/java/best_of_the_year/BestOfTheYearApplication.java
@@ -0,0 +1,13 @@
+package it.lessons.java.best_of_the_year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestOfTheYearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestOfTheYearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lessons/java/best_of_the_year/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lessons.java.best_of_the_year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
